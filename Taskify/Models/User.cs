f61086da59bb513b62bcf3bd210abc3ef701ae7e
Azido <Sulaimonabdulazeez02@gmail.com>
2024-12-05 22:50:45 +0100

    added the user endpoint

---
+++ Taskify/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskify.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string email { get; set; }
+        public string role { get; set; }
+    }
+}
---
+++ Taskify/Repository/UserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Taskify.Repository
+{
+    public class UserRepository
+    {
+    }
+}
